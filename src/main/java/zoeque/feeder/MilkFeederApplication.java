7b8553ffc6593ad589d4a308c4c324b732d1dd41
zoeque <121230485+zoeque@users.noreply.github.com>
2024-05-26 17:21:21 +0900

    add #1 - initial commit

---
+++ src/main/java/zoeque/feeder/MilkFeederApplication.java
@@ -0,0 +1,13 @@
+package zoeque.feeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkFeederApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilkFeederApplication.class, args);
+	}
+
+}
